a1efbd0827695c72419850f4500bc7b98b5ba015
Jamille Maria <124792344+millemaria@users.noreply.github.com>
2024-06-05 16:53:55 -0300

    update NavOptions

---
+++ components/NavOptions.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NavOptions = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default NavOptions
+
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
+import NavOptions from '../components/NavOptions';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style ={tw`bg-white h-full`}>
-      <View>
+      <View style={tw`p-5`}>
         <Image
             style={{
                 width: 100, 
@@ -16,6 +17,7 @@ const HomeScreen = () => {
                 uri: "https://links.papareact.com/gzs",
             }}
         />
+        <NavOptions />
       </View>
     </SafeAreaView>
   );
