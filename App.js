106c8fdb6d16f876c73090147d5e310ef14b7741
Jamille Maria <124792344+millemaria@users.noreply.github.com>
2024-06-05 16:35:57 -0300

    Inciando a HomeScreen

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider} from "react-redux";
 import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
     <Provider store={ store }> 
     <View style={styles.container}>
-      <Text>Lets build Uber!</Text>
+      <HomeScreen />
     </View>
     </Provider>
   );
