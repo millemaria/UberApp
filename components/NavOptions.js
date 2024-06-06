50a74e6a320e7d5f33cd0b17b9404786f6a90a49
Jamille Maria <124792344+millemaria@users.noreply.github.com>
2024-06-06 16:50:14 -0300

    uptdate NavOptions

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,13 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Text, TouchableOpacity, View } from 'react-native'
+import tw from 'tailwind-react-native-classnames';
 import React from 'react'
+
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    Image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order food",
+    Image: "https://links.papareact.com/28w",
+    screen: "EatsScreen",
+  },
+];
 
 const NavOptions = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
-  )
-}
+    <FlatList
+    data={data}
+    keyExtractor={(item) => item.id}
+    renderItem={({ item}) => (
+      <TouchableOpacity>
+        style={tw``}
+        <View>
+        <Image
+        style={{ width: 120, height: 120, resizeMode: "contain"}}
+        source={{ uri: item.Image}}
+        />
+        <Text>{item.title}</Text>
+        </View> 
+      </TouchableOpacity>
+  )}
+    />
+  );
+};
 
-export default NavOptions
-
+export default NavOptions;
