3a6ddf2c562902b15f9d54c50dce58f5f58fded6
Jamille Maria <124792344+millemaria@users.noreply.github.com>
2024-06-13 16:17:34 -0300

    update

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
+const { plugin } = require("twrnc");
+
 module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugin: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: "env.",
+        },
+    ],
+  ],
   };
 };
