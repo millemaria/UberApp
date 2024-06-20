f4eada088d480212161905611815f52e81d76763
Jamille Maria <124792344+millemaria@users.noreply.github.com>
2024-06-20 16:58:36 -0300

    open th map.screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,34 +3,48 @@ import React from 'react';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import {GOOGLE_MAPS_APIKEY} from "@env";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
-    <SafeAreaView style ={tw`bg-white h-full`}>
+    <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
         <Image
-            style={{
-                width: 100, 
-                height:100, 
-                resizeMode:"contain",
-            }}
-            source={{
-                uri: "https://links.papareact.com/gzs",
-            }}
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
         />
 
-          <GooglePlacesAutocomplete
-          
+        <GooglePlacesAutocomplete
           placeholder="Where From?"
           styles={{
             container: {
-              flex:0,
+              flex: 0,
             },
             textInput: {
               fontSize: 18,
             },
           }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+              location: details.geometry.location,
+              description: data.description
+            }));
+
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
           minLength={2}
           query={{
             key: GOOGLE_MAPS_APIKEY,
@@ -38,7 +52,7 @@ const HomeScreen = () => {
           }}
           nearbyPlacesAPI="GooglePlacesSearch"
           debounce={400}
-          />
+        />
 
         <NavOptions />
       </View>
@@ -46,10 +60,10 @@ const HomeScreen = () => {
   );
 };
 
-export default HomeScreen
+export default HomeScreen;
 
 const styles = StyleSheet.create({
-    text: {
-        color: "blue"
-    }
-})
+  text: {
+    color: "blue"
+  }
+});
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,19 @@
 import { View, Text } from 'react-native'
+import tw from 'tailwind-react-native-classnames'
 import React from 'react'
+import MapView from 'react-native-maps';
 
 const MapScreen = () => {
   return (
     <View>
       <Text>MapScreen</Text>
+
+      <View style={ tw`h-1/2`}>
+        <Map 
+        />
+      </View>
+
+      <View style={ tw`h-1/2`}></View>
     </View>
   )
 }
